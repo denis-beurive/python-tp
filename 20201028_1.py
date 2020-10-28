310213ae5c41c6f8211dd5714ef15fa432627104
denis-beurive <denis.beurive@gmail.com>
2020-10-28 15:53:24 +0100

    Add new exercises

--- 20201028_1.py
+++ 20201028_1.py
@@ -62,6 +62,8 @@ def npair(*in_args: int) -> typing.List[int]:
 
 
 print(npair(10, 20, 30, 40))
+t = (10, 11, 13, 15)
+print(npair(*t))
 
 # --------------------------------------------------------------
 # Create an iterable
@@ -96,6 +98,78 @@ print("Has __next__() ? {:s}".format("yes" if hasattr(MyIterable, '__next__') an
 for v in my_iterable:
     print(f"{v:d}")
 
+
+# --------------------------------------------------------------
+# Conversions
+# --------------------------------------------------------------
+
+# Tuple => List
+my_tuple = (10, 20, 30)
+print("my_tuple is a tuple? {:s}".format("yes" if isinstance(my_tuple, typing.Tuple) else "no"))
+my_list = [v for v in my_tuple]
+print("my_list is a list? {:s}".format("yes" if isinstance(my_list, typing.List) else "no"))
+print(repr(my_list))
+
+# List = Tuple
+my_tuple = tuple([1, 3, 10])
+print("my_tuple is a Tuple? {:s}".format("yes" if isinstance(my_tuple, typing.Tuple) else "no"))
+my_tuple = (list,)
+print("my_tuple is a Tuple? {:s}".format("yes" if isinstance(my_tuple, typing.Tuple) else "no"))
+my_tuple = tuple(v for v in [1, 3, 10])
+print("my_tuple is a Tuple? {:s}".format("yes" if isinstance(my_tuple, typing.Tuple) else "no"))
+
+# List => Generator
+my_generator = (v for v in my_list)
+print("my_generator is a generator? {:s}".format("yes" if isinstance(my_generator, typing.Generator) else "no"))
+print("my_generator is a list? {:s}".format("yes" if isinstance(my_generator, typing.List) else "no"))
+
+
+# --------------------------------------------------------------
+# Rot 13
+# --------------------------------------------------------------
+
+
+def rot13(in_string: str):
+    result_list: list = []
+    for c in list(in_string):
+        if 'A' <= c <= 'Z':
+            result_list.append(chr((ord(c)-ord('A')-13) % 26 + ord('A')))
+            continue
+        if 'a' <= c <= 'z':
+            result_list.append(chr((ord(c)-ord('a')-13) % 26 + ord('a')))
+            continue
+        result_list.append(c)
+    return "".join(result_list)
+
+
+print(rot13("NZ nz"))
+
+table1 = [c for c in range(0, ord('A'))]
+table2 = [chr((c-ord('A')-13) % 26 + ord('A')) for c in range(ord('A'), ord('Z')+1)]
+table3 = [c for c in range(ord('Z')+1, ord('a'))]
+table4 = [chr((c-ord('a')-13) % 26 + ord('a')) for c in range(ord('a'), ord('z')+1)]
+table5 = [c for c in range(ord('z')+1, 256)]
+
+table = [*table1, *table2, *table3, *table4, *table5]
+print("count: {:d}".format(table1.__len__()))
+print("count: {:d}".format(table2.__len__()))
+print("count: {:d}".format(table3.__len__()))
+print("count: {:d}".format(table4.__len__()))
+print("count: {:d}".format(table5.__len__()))
+print("count: {:d}".format(table.__len__()))
+print(table)
+
+
+def uwords(in_path: str) -> typing.List[str]:
+    fd: typing.TextIO = open(in_path, "r")
+    words: typing.Set[str] = set([])
+    for line in fd:
+        words.update(line.split())
+    fd.close()
+    return list(words)
+
+
+print(uwords("data.txt"))
 
 
 
