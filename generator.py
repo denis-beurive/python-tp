16b88f18595ccc19fae8bfeb4637d9bff571370d
denis-beurive <denis.beurive@gmail.com>
2020-11-02 09:23:30 +0100

    Good!

--- generator.py
+++ generator.py
@@ -2,6 +2,8 @@ import typing
 import sys
 
 
+# x, f(x), f(f(x)), f(f(f(x)))...
+
 def fgen(f: typing.Callable[[int], int], x: int) -> typing.Generator[int, None, None]:
     first: bool = True
     last_value: int = x
@@ -14,9 +16,32 @@ def fgen(f: typing.Callable[[int], int], x: int) -> typing.Generator[int, None, 
             yield last_value
 
 
+# x, f(x), f(f(x)), f(f(f(x)))...
+def fgen2(f: typing.Callable[[int], int], x: int) -> typing.Generator[int, None, None]:
+    def sub(f: typing.Callable[[int], int], x: int, count: int):
+        if 0 == count:
+            return x
+        else:
+            return f(x)
+
+    c: int = 0
+    while True:
+        v: int = sub(f, x, c)
+        yield v
+        c += 1
+        x = v
+
+
+def fgen3(f: typing.Callable[[typing.Any], typing.Any], x: typing.Any) -> typing.Generator[typing.Any, None, None]:
+    yield x
+    while True:
+        v: typing.Any = f(x)
+        yield v
+        x = v
+
 # Expected: 3, 2*3, 2*2*3, 2*2*2*3
 
 v: int
-for v in fgen(lambda x: 2*x, 3):
+for v in fgen3(lambda x: 2*x, 3):
     print(f"{v:d}")
     sys.stdout.flush()
