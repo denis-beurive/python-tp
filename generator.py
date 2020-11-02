4cb9ea686363c97e8e6738c1477e6950142babf5
denis-beurive <denis.beurive@gmail.com>
2020-11-02 09:53:32 +0100

    Good!

--- generator.py
+++ generator.py
@@ -32,16 +32,41 @@ def fgen2(f: typing.Callable[[int], int], x: int) -> typing.Generator[int, None,
         x = v
 
 
-def fgen3(f: typing.Callable[[typing.Any], typing.Any], x: typing.Any) -> typing.Generator[typing.Any, None, None]:
+def fgen3(f: typing.Callable[[typing.Any], typing.Any], x: typing.Any) \
+        -> typing.Generator[typing.Any, None, None]:
     yield x
     while True:
         v: typing.Any = f(x)
         yield v
         x = v
 
-# Expected: 3, 2*3, 2*2*3, 2*2*2*3
 
+def fgen4(f: typing.Callable[[typing.Any], typing.Any], x: typing.Any) \
+        -> typing.Generator[typing.Any, None, None]:
+    ff = lambda x: x
+    while True:
+        v = ff(x)
+        yield v
+        x = v
+        ff = f
+
+
+def fgen5(f: typing.Callable[[typing.Any], typing.Any], x: typing.Any) \
+        -> typing.Generator[typing.Any, None, None]:
+    for i in range(0, 100):
+        x = (x if i == 0 else f(x))
+        yield x
+
+
+def fgen6(f: typing.Callable[[typing.Any], typing.Any], x: typing.Any) \
+        -> typing.Generator[typing.Any, None, None]:
+    while True:
+        yield x
+        x = f(x)
+
+
+# Expected: 3, 2*3, 2*2*3, 2*2*2*3
 v: int
-for v in fgen3(lambda x: 2*x, 3):
+for v in fgen6(lambda x: 2*x, 3):
     print(f"{v:d}")
     sys.stdout.flush()
